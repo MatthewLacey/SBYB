3aca13271f3ba9fb0ef5620dc39cb59abab3e54d
Matthew Lacey <mlacey015@gmail.com>
2019-08-01 12:45:39 -0700

    alternativeBrands added to schema

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = require('./app');
 
 dotenv.config();
 
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -33,27 +35,50 @@ const brandSchema = new mongoose.Schema({
   },
   sustainabilityScore: {
     type: Number,
-    required: [true, 'Brand sustainability score missing']
+    required: [true, 'Brand sustainability score missing'],
+    default: 0
   },
   cultureScore: {
     type: Number,
-    required: [true, 'Brand culture score missing']
+    required: [true, 'Brand culture score missing'],
+    default: 0
   },
   politicalScore: {
     type: Number,
-    required: [true, 'Brand political score missing']
+    required: [true, 'Brand political score missing'],
+    default: 0
   },
   parent: {
-    type: ObjectID,
+    type: ObjectId,
     required: false
   },
   subsidiaries: {
-    type: [ObjectID],
+    type: [ObjectId],
     required: false
   }
 });
 
 const Brand = mongoose.model('Brand', brandSchema);
+
+// const testBrand = new Brand({
+//   name: 'Anheuser-Busch Inbev SA',
+//   logo:
+//     'https://en.wikipedia.org/wiki/Anheuser-Busch_InBev#/media/File:AB_InBev_logo.svg',
+//   industry: 'BEVERAGES',
+//   num_employees: 182915,
+//   sustainabilityScore: 5,
+//   cultureScore: 8,
+//   politicalScore: 5
+// });
+
+// testBrand
+//   .save()
+//   .then(doc => {
+//     console.log(doc);
+//   })
+//   .catch(err => {
+//     console.log('ERROR 💥');
+//   });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
