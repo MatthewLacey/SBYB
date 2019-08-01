3a94ece3ebce02328ffbb66156a4807481fb32a5
Matthew Lacey <mlacey015@gmail.com>
2019-07-31 22:15:46 -0700

    mongoose

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config();
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
