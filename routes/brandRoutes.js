a51ac75e7bc73ddfa2e2d120bdeec58f5f5d69e1
Matthew Lacey <mlacey015@gmail.com>
2019-08-01 13:08:00 -0700

    refactored models

--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -3,8 +3,6 @@ const brandController = require('./../controllers/brandController');
 
 const router = express.Router();
 
-router.param('id', brandController.checkID);
-
 router.route('/').get(brandController.getAllBrands);
 
 router.route('/:id').get(brandController.getBrand);
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = require('./app');
 
 dotenv.config();
 
-const ObjectId = mongoose.Schema.Types.ObjectId;
-
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -13,76 +11,6 @@ mongoose
     useFindAndModify: false
   })
   .then(() => console.log('DB connection successful'));
-
-const brandSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Brand name missing'],
-    unique: true
-  },
-  logo: {
-    type: String,
-    required: false,
-    unique: true
-  },
-  industry: {
-    type: String,
-    required: [true, 'Brand industry type missing']
-  },
-  numEmployees: {
-    type: Number,
-    required: false
-  },
-  sustainabilityScore: {
-    type: Number,
-    required: [true, 'Brand sustainability score missing'],
-    default: 0
-  },
-  cultureScore: {
-    type: Number,
-    required: [true, 'Brand culture score missing'],
-    default: 0
-  },
-  politicalScore: {
-    type: Number,
-    required: [true, 'Brand political score missing'],
-    default: 0
-  },
-  parent: {
-    type: ObjectId,
-    required: false
-  },
-  subsidiaries: {
-    type: [ObjectId],
-    required: false
-  },
-  alternativeBrands: {
-    type: [ObjectId],
-    required: false
-  }
-});
-
-const Brand = mongoose.model('Brand', brandSchema);
-
-// const testBrand = new Brand({
-//   name: 'Anheuser-Busch Inbev SA',
-//   logo:
-//     'https://en.wikipedia.org/wiki/Anheuser-Busch_InBev#/media/File:AB_InBev_logo.svg',
-//   industry: 'BEVERAGES',
-//   num_employees: 182915,
-//   sustainabilityScore: 5,
-//   cultureScore: 8,
-//   politicalScore: 5
-// });
-
-// testBrand
-//   .save()
-//   .then(doc => {
-//     console.log(doc);
-//   })
-//   .catch(err => {
-//     console.log('ERROR 💥');
-//   });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
