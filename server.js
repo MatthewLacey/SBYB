c053db7f5042e011e318dc09de10cfb0c54b229f
Matthew Lacey <mlacey015@gmail.com>
2019-08-01 00:14:37 -0700

    brand schema created

--- server.js
+++ server.js
@@ -12,6 +12,49 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
+const brandSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Brand name missing'],
+    unique: true
+  },
+  logo: {
+    type: String,
+    required: false,
+    unique: true
+  },
+  industry: {
+    type: String,
+    required: [true, 'Brand industry type missing']
+  },
+  numEmployees: {
+    type: Number,
+    required: false
+  },
+  sustainabilityScore: {
+    type: Number,
+    required: [true, 'Brand sustainability score missing']
+  },
+  cultureScore: {
+    type: Number,
+    required: [true, 'Brand culture score missing']
+  },
+  politicalScore: {
+    type: Number,
+    required: [true, 'Brand political score missing']
+  },
+  parent: {
+    type: ObjectID,
+    required: false
+  },
+  subsidiaries: {
+    type: [ObjectID],
+    required: false
+  }
+});
+
+const Brand = mongoose.model('Brand', brandSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
